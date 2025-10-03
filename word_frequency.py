41483ea43062afc8116dc192978e6b90b21ed116
Alen Thomas <alen@uoguelph.ca>
2025-10-03 12:19:41 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -40,12 +40,12 @@ words = user_sentence.split()
 word_list = []
 frequency_list = []
 
-#for word in words:
-cleaned = words.strip(punctuation).lower()
-if cleaned in word_list:
+for word in words:
+    cleaned = words.strip(punctuation).lower()
+    if cleaned in word_list:
         index = word_list.index(cleaned)
         frequency_list[index] += 1
-else:
+    else:
         word_list.append(cleaned)
         frequency_list.append(1)
 
